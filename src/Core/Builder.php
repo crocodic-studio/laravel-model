3f5bbf3d47bebf51833160839a8139bb793f0c1c
Ferry Ariawan <ferry@crocodic.com>
2021-03-10 09:19:00 +0700

    Fix builder

--- src/Core/Builder.php
+++ src/Core/Builder.php
@@ -6,8 +6,8 @@ namespace Crocodic\LaravelModel\Core;
 /**
  * Class Builder
  * @package Crocodic\LaravelModel\Core
- * @method \Illuminate\Database\Query\Builder addSelectTable(string $table)
- * @method \Illuminate\Database\Query\Builder withTable($table)
+ * @method Builder addSelectTable(string $table)
+ * @method Builder withTable($table)
  */
 abstract class Builder extends \Illuminate\Database\Query\Builder
 {
