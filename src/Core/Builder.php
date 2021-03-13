199499396f3f90cf1313e7878177f6de33c8141e
Ferry Ariawan <ferry@crocodic.com>
2021-03-13 12:00:36 +0700

    Add like

--- src/Core/Builder.php
+++ src/Core/Builder.php
@@ -8,6 +8,7 @@ namespace Crocodic\LaravelModel\Core;
  * @package Crocodic\LaravelModel\Core
  * @method Builder addSelectTable(string $table)
  * @method Builder withTable($table)
+ * @method Builder like($column, $keyword)
  */
 abstract class Builder extends \Illuminate\Database\Query\Builder
 {
--- src/Helpers/BuilderMacro.php
+++ src/Helpers/BuilderMacro.php
@@ -17,7 +17,7 @@ class BuilderMacro
         });
 
         Builder::macro("withTable", function($table) {
-            /** @var Builder $this */
+            /** @var \Crocodic\LaravelModel\Core\Builder $this */
             if(is_array($table)) {
                 foreach($table as $tbl) {
                     $this->leftJoin($tbl,$tbl.".id","=",$this->from."_id");
@@ -28,6 +28,14 @@ class BuilderMacro
                 $this->addSelectTable($table);
             }
             return $this;
+        });
+
+        Builder::macro("like", function($column, $keyword) {
+            /** @var \Crocodic\LaravelModel\Core\Builder $this */
+            if(substr($keyword,0,1) != "%" && substr($keyword,-1,1) != "%") {
+                $keyword = "%".$keyword."%";
+            }
+            $this->whereRaw($column." like '".$keyword."'");
         });
     }
 }
